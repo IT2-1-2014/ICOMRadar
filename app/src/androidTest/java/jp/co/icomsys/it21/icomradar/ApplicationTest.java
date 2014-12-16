88434858d8b1802a2eb30b2d3a59ead98718e7a9
Masaharu Hayashi <hayashi.masaharu@gmail.com>
2014-12-16 10:45:16 +0900

    first commit

---
+++ app/src/androidTest/java/jp/co/icomsys/it21/icomradar/ApplicationTest.java
@@ -0,0 +1,31 @@
+package jp.co.icomsys.it21.icomradar;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class ApplicationTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void teardrop() {
+
+    }
+
+    @Test
+    public void testSample() {
+        Assert.assertEquals("a", "a");
+
+    }
+}
